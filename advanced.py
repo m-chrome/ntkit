53431154d29e6b4389976b9487a1c72ec9303c89
Evgenii Potapov <allforblender@mail.ru>
2018-01-27 19:08:02 +0300

    + Add full factorization
    Add full factorization method and an
    "advanced" category. Also fixed Fermat
    factorization method, now it works with
    even numbers.

---
+++ advanced.py
@@ -0,0 +1,24 @@
+import factorization
+import primality_tests
+
+
+def full_factorize(number, fact_method=factorization.fermat_method,
+                   primality_method=primality_tests.miller_rabin_primality_test):
+    to_verify = [number]
+    factorize_list = []
+    while to_verify:
+        candidate = to_verify.pop()
+        if primality_method(candidate):
+            factorize_list.append(candidate)
+        else:
+            if candidate % 2 == 0:
+                p, q = 2, candidate // 2
+            else:
+                p, q = fact_method(candidate)
+            to_verify.append(p)
+            to_verify.append(q)
+    primes = set(factorize_list)
+    vision = []
+    for prime in primes:
+        vision.append((prime, factorize_list.count(prime)))
+    return tuple(sorted(vision))
